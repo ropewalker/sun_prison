4bd1b0c9142125bc16d11381db55e313c4b9d847
Dima Lazarev <dmitry.lazarev@gmail.com>
2020-08-30 20:46:55 +0300

    Disable (for now) coloring of tiles based on sun movement

--- src/systems/rendering/tile_sprite_update_system.rs
+++ src/systems/rendering/tile_sprite_update_system.rs
@@ -13,10 +13,10 @@ pub fn tile_sprite_update_system(
 fn insolation_to_sprite_index(insolation: &Insolation, is_highlighted: &IsHighlighted) -> u32 {
     match (insolation, is_highlighted.0) {
         (Insolation::Day, false) => 0,
-        (Insolation::Twilight, false) => 1,
-        (Insolation::Night, false) => 2,
+        (Insolation::Twilight, false) => 0,
+        (Insolation::Night, false) => 0,
         (Insolation::Day, true) => 3,
-        (Insolation::Twilight, true) => 4,
-        (Insolation::Night, true) => 5,
+        (Insolation::Twilight, true) => 3,
+        (Insolation::Night, true) => 3,
     }
 }
