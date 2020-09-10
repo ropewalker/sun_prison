c0de2cd2b80d05fbf28b8ef957d1c515e0ea4603
Dima Lazarev <dmitry.lazarev@gmail.com>
2020-09-10 22:32:19 +0300

    Refactor player movement system

--- src/systems/player_movement_system.rs
+++ src/systems/player_movement_system.rs
@@ -3,30 +3,27 @@ use crate::components::*;
 use crate::resources::*;
 use bevy::prelude::*;
 
-fn next_tile(current_tile: GameCoordinates, direction: Vector3) -> GameCoordinates {
-    let new_cubelet_position = current_tile.cubelet_position.0 + direction;
+fn make_move(
+    cubelet_position: &mut Mut<CubeletPosition>,
+    normal_orientation: &mut Mut<NormalOrientation>,
+    tangent_orientation: &mut Mut<TangentOrientation>,
+    direction: Vector3,
+) {
+    let new_cubelet_position = cubelet_position.0 + direction;
 
     if new_cubelet_position.x.abs() > PLANET_RADIUS
         || new_cubelet_position.y.abs() > PLANET_RADIUS
         || new_cubelet_position.z.abs() > PLANET_RADIUS
     {
-        GameCoordinates {
-            cubelet_position: current_tile.cubelet_position,
-            normal_orientation: NormalOrientation(direction),
-            tangent_orientation: TangentOrientation(
-                match current_tile.tangent_orientation.0.dot(direction) {
-                    x if x > 0 => -current_tile.normal_orientation.0,
-                    x if x < 0 => current_tile.normal_orientation.0,
-                    _ => current_tile.tangent_orientation.0,
-                },
-            ),
-        }
+        tangent_orientation.0 = match tangent_orientation.0.dot(direction) {
+            x if x > 0 => -normal_orientation.0,
+            x if x < 0 => normal_orientation.0,
+            _ => tangent_orientation.0,
+        };
+
+        normal_orientation.0 = direction;
     } else {
-        GameCoordinates {
-            cubelet_position: CubeletPosition(new_cubelet_position),
-            normal_orientation: current_tile.normal_orientation,
-            tangent_orientation: current_tile.tangent_orientation,
-        }
+        cubelet_position.0 = new_cubelet_position;
     }
 }
 
@@ -40,35 +37,25 @@ pub fn player_movement_system(
 ) {
     if current_turn.side == GameSide::Player {
         if keyboard_input.just_pressed(KeyCode::Up) || keyboard_input.just_pressed(KeyCode::W) {
-            let new_coordinates = next_tile(
-                GameCoordinates {
-                    cubelet_position: *cubelet_position,
-                    normal_orientation: *normal_orientation,
-                    tangent_orientation: *tangent_orientation,
-                },
-                tangent_orientation.0,
+            let direction = tangent_orientation.0;
+            make_move(
+                &mut cubelet_position,
+                &mut normal_orientation,
+                &mut tangent_orientation,
+                direction,
             );
-
-            *cubelet_position = new_coordinates.cubelet_position;
-            *normal_orientation = new_coordinates.normal_orientation;
-            *tangent_orientation = new_coordinates.tangent_orientation;
 
             current_turn.side = GameSide::Sun;
         } else if keyboard_input.just_pressed(KeyCode::Down)
             || keyboard_input.just_pressed(KeyCode::S)
         {
-            let new_coordinates = next_tile(
-                GameCoordinates {
-                    cubelet_position: *cubelet_position,
-                    normal_orientation: *normal_orientation,
-                    tangent_orientation: *tangent_orientation,
-                },
-                -tangent_orientation.0,
+            let direction = -tangent_orientation.0;
+            make_move(
+                &mut cubelet_position,
+                &mut normal_orientation,
+                &mut tangent_orientation,
+                direction,
             );
-
-            *cubelet_position = new_coordinates.cubelet_position;
-            *normal_orientation = new_coordinates.normal_orientation;
-            *tangent_orientation = new_coordinates.tangent_orientation;
 
             current_turn.side = GameSide::Sun;
         } else if keyboard_input.just_pressed(KeyCode::Left)
