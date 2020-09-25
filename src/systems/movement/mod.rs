f00fa055c09038ef47c8aa44c4cd8bd4d9431617
Dima Lazarev <dmitry.lazarev@gmail.com>
2020-09-25 22:59:20 +0300

    Refactor movement systems

--- src/systems/movement/mod.rs
+++ src/systems/movement/mod.rs
@@ -2,4 +2,55 @@ mod enemies_movement_system;
 mod player_movement_system;
 mod sun_movement_system;
 
+use crate::algebra::*;
+use crate::components::*;
+use crate::resources::*;
+use bevy::prelude::*;
+
 pub use self::{enemies_movement_system::*, player_movement_system::*, sun_movement_system::*};
+
+pub fn make_move(coordinates: &mut Mut<GameCoordinates>, direction: UnitVector) {
+    let new_cubelet = coordinates.cubelet + direction;
+
+    if new_cubelet.x.abs() > PLANET_RADIUS
+        || new_cubelet.y.abs() > PLANET_RADIUS
+        || new_cubelet.z.abs() > PLANET_RADIUS
+    {
+        if let Some(tangent) = coordinates.tangent {
+            if tangent == direction {
+                coordinates.tangent = Some(-coordinates.normal)
+            } else if tangent == -direction {
+                coordinates.tangent = Some(coordinates.normal)
+            }
+        }
+
+        coordinates.normal = direction;
+    } else {
+        coordinates.cubelet = new_cubelet;
+    }
+}
+
+pub fn next_tile(position: &Position, direction: UnitVector) -> (Position, UnitVector) {
+    let new_cubelet = position.cubelet + direction;
+
+    if new_cubelet.x.abs() > PLANET_RADIUS
+        || new_cubelet.y.abs() > PLANET_RADIUS
+        || new_cubelet.z.abs() > PLANET_RADIUS
+    {
+        (
+            Position {
+                cubelet: position.cubelet,
+                normal: direction,
+            },
+            -position.normal,
+        )
+    } else {
+        (
+            Position {
+                cubelet: new_cubelet,
+                normal: position.normal,
+            },
+            direction,
+        )
+    }
+}
--- src/systems/movement/player_movement_system.rs
+++ src/systems/movement/player_movement_system.rs
@@ -1,54 +1,9 @@
+use super::*;
 use crate::algebra::*;
 use crate::components::*;
 use crate::resources::*;
 use bevy::prelude::*;
 use std::collections::HashMap;
-
-fn make_move(coordinates: &mut Mut<GameCoordinates>, direction: UnitVector) {
-    let new_cubelet = coordinates.cubelet + direction;
-
-    if new_cubelet.x.abs() > PLANET_RADIUS
-        || new_cubelet.y.abs() > PLANET_RADIUS
-        || new_cubelet.z.abs() > PLANET_RADIUS
-    {
-        if let Some(tangent) = coordinates.tangent {
-            if tangent == direction {
-                coordinates.tangent = Some(-coordinates.normal)
-            } else if tangent == -direction {
-                coordinates.tangent = Some(coordinates.normal)
-            }
-        }
-
-        coordinates.normal = direction;
-    } else {
-        coordinates.cubelet = new_cubelet;
-    }
-}
-
-fn next_tile(position: &Position, direction: UnitVector) -> (Position, UnitVector) {
-    let new_cubelet = position.cubelet + direction;
-
-    if new_cubelet.x.abs() > PLANET_RADIUS
-        || new_cubelet.y.abs() > PLANET_RADIUS
-        || new_cubelet.z.abs() > PLANET_RADIUS
-    {
-        (
-            Position {
-                cubelet: position.cubelet,
-                normal: direction,
-            },
-            -position.normal,
-        )
-    } else {
-        (
-            Position {
-                cubelet: new_cubelet,
-                normal: position.normal,
-            },
-            direction,
-        )
-    }
-}
 
 pub fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
