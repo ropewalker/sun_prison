f2792aa5766b0ee9b5b6920fca89628007c8384f
Dima Lazarev <dmitry.lazarev@gmail.com>
2020-11-02 21:00:15 +0200

    Reorganize systems
    Move rotation module inside movement module, which (sort of) addresses #16.

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,7 +1,6 @@
 mod movement;
 mod rendering;
-mod rotation;
 mod setup;
 mod viewshed_update_system;
 
-pub use self::{movement::*, rendering::*, rotation::*, setup::*, viewshed_update_system::*};
+pub use self::{movement::*, rendering::*, setup::*, viewshed_update_system::*};
